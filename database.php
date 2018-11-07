ad355a6c903f8a97f798d5d84fcad7ccca177308
Utsab <utsab.k.saha@gmail.com>
2018-11-07 06:20:39 +0000

    Add initial files

---
+++ database.php
@@ -0,0 +1,26 @@
+<?php
+// connect to our mysql database server
+function getDatabaseConnection() {
+    if (strpos($_SERVER['SERVER_NAME'], "c9users") !== false) {
+        // running on cloud9
+        $host = "localhost";
+        $username = "utsab";
+        $password = "cst336"; // best practice: define this in a separte file
+        $dbname = "memes_v2"; 
+    } else {
+        // running on Heroku
+        $host = "";
+        $username = "";
+        $password = ""; 
+        $dbname = ""; 
+    }
+    
+    
+    // Create connection
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbConn; 
+}
+// temporary test code
+//$dbConn = getDatabaseConnection(); 
+?>
---
+++ navigation.php
@@ -0,0 +1,18 @@
+<?php
+  session_start(); 
+?>
+<nav>
+    <ul>
+     <?php 
+        if (isset($_SESSION['user_id'])) {
+          echo '<li> <a href="index.php"> Home </a> </li> '; 
+          echo '<li> <a href="memes.php">Memes</a></li>'; 
+          echo '<li> <a href="logout.php">Logout</a></li>'; 
+        }
+        else  
+          echo '<li> <a href="login.php">Login</a></li>'; 
+    ?>
+
+    </ul>
+    <div class="clear"></div>
+</nav>
